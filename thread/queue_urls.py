b359bea7d0c79742fb45f40ccd60540e5e6ee478
dev.yu85 <dev.yu85@yandex.ru>
2017-12-20 22:01:16 +0300

    renamed to queue_by_threads added threads to queue_urls

--- thread/queue_urls.py
+++ thread/queue_urls.py
@@ -1,20 +1,23 @@
 import urllib.request as request
 import time
+import queue
+import threading
 
 urls = ["lenta.ru", "stoloto.ru", "auto.ru",
         "tproger.ru", "pikabu.ru", "python.org"]
+max_threads = 2
 
 
 def some_work(url):
     name = url
-    print(name)
+    # print("Found work ", name)
     try:
         response = request.urlopen(r"http://" + url)
     except Exception as e:
-        print("{} Error: {}".format(name,e))
+        print("{}: {}".format(name, e))
         return
 
-    file = "./output." + name
+    file = "./output/" + name + ".txt"
     f_out = open(file, "wb")
     f_out.write(response.read())
     f_out.close()
@@ -25,6 +28,39 @@ def sequence_of_works():
         some_work(url)
 
 
+class GoatWorker(threading.Thread):
+    def __init__(self, q):
+        super().__init__()
+        self.queue = q
+
+    def run(self):
+        while True:
+            item = self.queue.get()
+            if item is None:
+                break
+            # print("{} found {}".format(threading.current_thread(), item))
+            some_work(item)
+            self.queue.task_done()
+
+
+def work_by_threads(number_of_threads):
+    print("Threads are ", number_of_threads)
+    threads = []
+    lq = queue.Queue()
+    for u in urls:
+        lq.put(u)
+    for i in range(number_of_threads):
+        t = GoatWorker(lq)
+        threads.append(t)
+        t.start()
+    # print("Main thread. Waiting...")
+    lq.join()
+    for t in threads:
+        lq.put(None)
+    for t in threads:
+        t.join()
+
+
 if __name__ == "__main__":
     t0 = time.time()
     print("{}: Sequence works ... ".format(
@@ -32,4 +68,17 @@ if __name__ == "__main__":
     sequence_of_works()
     print("{}: Sequence finished by {} secs.".format(
         time.ctime(t0), time.time() - t0))
+    print("\n" * 3)
+    time.sleep(3)
+
+    workers = [2, 3, 4]
+
+    for worker in workers:
+        t0 = time.time()
+        work_by_threads(worker)
+        print("{}: Threads finished by {} secs.".format(
+            time.ctime(t0), time.time() - t0))
+        print("\n" * 3)
+        time.sleep(3)
+
     print("Bye Bye!!!")
